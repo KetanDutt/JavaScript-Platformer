1e26c21f1bc26464bb2267ed1a337ded4f49761e
Ketan Dutt <amaze.gamez1@gmail.com>
2025-04-29 19:18:57 +0530

    level json seperated

--- engine.js
+++ engine.js
@@ -1,21 +1,21 @@
 var Engine = function () {
 
-    this.alert_errors   = false;
-    this.log_info       = true;
-    this.tile_size      = 16;
+    this.alert_errors = false;
+    this.log_info = true;
+    this.tile_size = 16;
     this.limit_viewport = true;
-    this.jump_switch    = 0;
-    
+    this.jump_switch = 0;
+
     this.viewport = {
         x: 200,
         y: 200
     };
-    
+
     this.camera = {
         x: 0,
         y: 0
     };
-    
+
     this.key = {
         left: false,
         right: false,
@@ -28,17 +28,17 @@ var Engine = function () {
             x: 0,
             y: 0
         },
-        
+
         vel: {
             x: 0,
             y: 0
         },
-        
+
         can_jump: true
     };
 
     window.onkeydown = this.keydown.bind(this);
-    window.onkeyup   = this.keyup.bind(this);
+    window.onkeyup = this.keyup.bind(this);
 };
 
 Engine.prototype.error = function (message) {
@@ -63,15 +63,15 @@ Engine.prototype.keydown = function (e) {
     var _this = this;
 
     switch (e.keyCode) {
-    case 37:
-        _this.key.left = true;
-        break;
-    case 38:
-        _this.key.up = true;
-        break;
-    case 39:
-        _this.key.right = true;
-        break;
+        case 37:
+            _this.key.left = true;
+            break;
+        case 38:
+            _this.key.up = true;
+            break;
+        case 39:
+            _this.key.right = true;
+            break;
     }
 };
 
@@ -80,23 +80,23 @@ Engine.prototype.keyup = function (e) {
     var _this = this;
 
     switch (e.keyCode) {
-    case 37:
-        _this.key.left = false;
-        break;
-    case 38:
-        _this.key.up = false;
-        break;
-    case 39:
-        _this.key.right = false;
-        break;
+        case 37:
+            _this.key.left = false;
+            break;
+        case 38:
+            _this.key.up = false;
+            break;
+        case 39:
+            _this.key.right = false;
+            break;
     }
 };
 
 Engine.prototype.load_map = function (map) {
 
-    if (typeof map      === 'undefined'
-     || typeof map.data === 'undefined'
-     || typeof map.keys === 'undefined') {
+    if (typeof map === 'undefined'
+        || typeof map.data === 'undefined'
+        || typeof map.keys === 'undefined') {
 
         this.error('Error: Invalid map data!');
 
@@ -106,22 +106,22 @@ Engine.prototype.load_map = function (map) {
     this.current_map = map;
 
     this.current_map.background = map.background || '#333';
-    this.current_map.gravity = map.gravity || {x: 0, y: 0.3};
+    this.current_map.gravity = map.gravity || { x: 0, y: 0.3 };
     this.tile_size = map.tile_size || 16;
 
     var _this = this;
-    
+
     this.current_map.width = 0;
     this.current_map.height = 0;
 
     map.keys.forEach(function (key) {
 
         map.data.forEach(function (row, y) {
-            
+
             _this.current_map.height = Math.max(_this.current_map.height, y);
 
             row.forEach(function (tile, x) {
-                
+
                 _this.current_map.width = Math.max(_this.current_map.width, x);
 
                 if (tile == key.id)
@@ -129,19 +129,19 @@ Engine.prototype.load_map = function (map) {
             });
         });
     });
-    
+
     this.current_map.width_p = this.current_map.width * this.tile_size;
     this.current_map.height_p = this.current_map.height * this.tile_size;
 
     this.player.loc.x = map.player.x * this.tile_size || 0;
     this.player.loc.y = map.player.y * this.tile_size || 0;
     this.player.colour = map.player.colour || '#000';
-    
+
     this.camera = {
         x: 0,
         y: 0
     };
-    
+
     this.player.vel = {
         x: 0,
         y: 0
@@ -180,12 +180,12 @@ Engine.prototype.draw_map = function (context, fore) {
 
                 var t_x = (x * this.tile_size) - this.camera.x;
                 var t_y = (y * this.tile_size) - this.camera.y;
-                
-                if(t_x < -this.tile_size
-                || t_y < -this.tile_size
-                || t_x > this.viewport.x
-                || t_y > this.viewport.y) continue;
-                
+
+                if (t_x < -this.tile_size
+                    || t_y < -this.tile_size
+                    || t_x > this.viewport.x
+                    || t_y > this.viewport.y) continue;
+
                 this.draw_tile(
                     t_x,
                     t_y,
@@ -210,60 +210,60 @@ Engine.prototype.move_player = function () {
         Math.round(this.player.loc.x / this.tile_size),
         Math.round(this.player.loc.y / this.tile_size)
     );
-     
-    if(tile.gravity) {
-        
+
+    if (tile.gravity) {
+
         this.player.vel.x += tile.gravity.x;
         this.player.vel.y += tile.gravity.y;
-        
+
     } else {
-        
+
         this.player.vel.x += this.current_map.gravity.x;
         this.player.vel.y += this.current_map.gravity.y;
     }
-    
+
     if (tile.friction) {
 
         this.player.vel.x *= tile.friction.x;
         this.player.vel.y *= tile.friction.y;
     }
 
-    var t_y_up   = Math.floor(tY / this.tile_size);
+    var t_y_up = Math.floor(tY / this.tile_size);
     var t_y_down = Math.ceil(tY / this.tile_size);
-    var y_near1  = Math.round((this.player.loc.y - offset) / this.tile_size);
-    var y_near2  = Math.round((this.player.loc.y + offset) / this.tile_size);
+    var y_near1 = Math.round((this.player.loc.y - offset) / this.tile_size);
+    var y_near2 = Math.round((this.player.loc.y + offset) / this.tile_size);
 
-    var t_x_left  = Math.floor(tX / this.tile_size);
+    var t_x_left = Math.floor(tX / this.tile_size);
     var t_x_right = Math.ceil(tX / this.tile_size);
-    var x_near1   = Math.round((this.player.loc.x - offset) / this.tile_size);
-    var x_near2   = Math.round((this.player.loc.x + offset) / this.tile_size);
+    var x_near1 = Math.round((this.player.loc.x - offset) / this.tile_size);
+    var x_near2 = Math.round((this.player.loc.x + offset) / this.tile_size);
 
-    var top1    = this.get_tile(x_near1, t_y_up);
-    var top2    = this.get_tile(x_near2, t_y_up);
+    var top1 = this.get_tile(x_near1, t_y_up);
+    var top2 = this.get_tile(x_near2, t_y_up);
     var bottom1 = this.get_tile(x_near1, t_y_down);
     var bottom2 = this.get_tile(x_near2, t_y_down);
-    var left1   = this.get_tile(t_x_left, y_near1);
-    var left2   = this.get_tile(t_x_left, y_near2);
-    var right1  = this.get_tile(t_x_right, y_near1);
-    var right2  = this.get_tile(t_x_right, y_near2);
+    var left1 = this.get_tile(t_x_left, y_near1);
+    var left2 = this.get_tile(t_x_left, y_near2);
+    var right1 = this.get_tile(t_x_right, y_near1);
+    var right2 = this.get_tile(t_x_right, y_near2);
 
 
     if (tile.jump && this.jump_switch > 15) {
 
         this.player.can_jump = true;
-        
+
         this.jump_switch = 0;
-        
+
     } else this.jump_switch++;
-    
+
     this.player.vel.x = Math.min(Math.max(this.player.vel.x, -this.current_map.vel_limit.x), this.current_map.vel_limit.x);
     this.player.vel.y = Math.min(Math.max(this.player.vel.y, -this.current_map.vel_limit.y), this.current_map.vel_limit.y);
-    
+
     this.player.loc.x += this.player.vel.x;
     this.player.loc.y += this.player.vel.y;
-    
+
     this.player.vel.x *= .9;
-    
+
     if (left1.solid || left2.solid || right1.solid || right2.solid) {
 
         /* fix overlap */
@@ -286,13 +286,13 @@ Engine.prototype.move_player = function () {
         if (right2.solid && right2.bounce > bounce) bounce = right2.bounce;
 
         this.player.vel.x *= -bounce || 0;
-        
+
     }
-    
+
     if (top1.solid || top2.solid || bottom1.solid || bottom2.solid) {
 
         /* fix overlap */
-        
+
         while (this.get_tile(x_near1, Math.floor(this.player.loc.y / this.tile_size)).solid
             || this.get_tile(x_near2, Math.floor(this.player.loc.y / this.tile_size)).solid)
             this.player.loc.y += 0.1;
@@ -302,48 +302,48 @@ Engine.prototype.move_player = function () {
             this.player.loc.y -= 0.1;
 
         /* tile bounce */
-        
+
         var bounce = 0;
-        
+
         if (top1.solid && top1.bounce > bounce) bounce = top1.bounce;
         if (top2.solid && top2.bounce > bounce) bounce = top2.bounce;
         if (bottom1.solid && bottom1.bounce > bounce) bounce = bottom1.bounce;
         if (bottom2.solid && bottom2.bounce > bounce) bounce = bottom2.bounce;
-        
+
         this.player.vel.y *= -bounce || 0;
 
         if ((bottom1.solid || bottom2.solid) && !tile.jump) {
-            
+
             this.player.on_floor = true;
             this.player.can_jump = true;
         }
-        
+
     }
-    
+
     // adjust camera
 
-    var c_x = Math.round(this.player.loc.x - this.viewport.x/2);
-    var c_y = Math.round(this.player.loc.y - this.viewport.y/2);
+    var c_x = Math.round(this.player.loc.x - this.viewport.x / 2);
+    var c_y = Math.round(this.player.loc.y - this.viewport.y / 2);
     var x_dif = Math.abs(c_x - this.camera.x);
     var y_dif = Math.abs(c_y - this.camera.y);
-    
-    if(x_dif > 5) {
-        
+
+    if (x_dif > 5) {
+
         var mag = Math.round(Math.max(1, x_dif * 0.1));
-    
-        if(c_x != this.camera.x) {
-            
+
+        if (c_x != this.camera.x) {
+
             this.camera.x += c_x > this.camera.x ? mag : -mag;
-            
-            if(this.limit_viewport) {
-                
-                this.camera.x = 
+
+            if (this.limit_viewport) {
+
+                this.camera.x =
                     Math.min(
                         this.current_map.width_p - this.viewport.x + this.tile_size,
                         this.camera.x
                     );
-                
-                this.camera.x = 
+
+                this.camera.x =
                     Math.max(
                         0,
                         this.camera.x
@@ -351,24 +351,24 @@ Engine.prototype.move_player = function () {
             }
         }
     }
-    
-    if(y_dif > 5) {
-        
+
+    if (y_dif > 5) {
+
         var mag = Math.round(Math.max(1, y_dif * 0.1));
-        
-        if(c_y != this.camera.y) {
-            
+
+        if (c_y != this.camera.y) {
+
             this.camera.y += c_y > this.camera.y ? mag : -mag;
-        
-            if(this.limit_viewport) {
-                
-                this.camera.y = 
+
+            if (this.limit_viewport) {
+
+                this.camera.y =
                     Math.min(
                         this.current_map.height_p - this.viewport.y + this.tile_size,
                         this.camera.y
                     );
-                
-                this.camera.y = 
+
+                this.camera.y =
                     Math.max(
                         0,
                         this.camera.y
@@ -376,12 +376,12 @@ Engine.prototype.move_player = function () {
             }
         }
     }
-    
-    if(this.last_tile != tile.id && tile.script) {
-    
+
+    if (this.last_tile != tile.id && tile.script) {
+
         eval(this.current_map.scripts[tile.script]);
     }
-    
+
     this.last_tile = tile.id;
 };
 
@@ -396,7 +396,7 @@ Engine.prototype.update_player = function () {
     if (this.key.up) {
 
         if (this.player.can_jump && this.player.vel.y > -this.current_map.vel_limit.y) {
-            
+
             this.player.vel.y -= this.current_map.movement_speed.jump;
             this.player.can_jump = false;
         }
@@ -429,7 +429,6 @@ Engine.prototype.draw_player = function (context) {
 };
 
 Engine.prototype.update = function () {
-
     this.update_player();
 };
 
